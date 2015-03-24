b51b75ff4024c9bd41c0a05b3edd96b017744de6
Andrew <theawesomecoder61@users.noreply.github.com>
2015-03-23 23:05:55 -0400

    Update TodayViewController.h

--- Pointless Widget/TodayViewController.h
+++ Pointless Widget/TodayViewController.h
@@ -2,7 +2,7 @@
 //  TodayViewController.h
 //  Pointless Widget
 //
-//  Created by Andrew Mellen on 3/17/15.
+//  Created by Andrew M on 3/17/15.
 //  Copyright (c) 2015 theawesomecoder61. All rights reserved.
 //
 
